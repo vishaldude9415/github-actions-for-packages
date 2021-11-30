0bd295806d1d8d4dbd0b786643d5e974867283db
vishaldude9415 <vishaldude9415@gmail.com>
2021-11-30 19:20:53 +0530

    This is commit

--- main (1).cpp
+++ main (1).cpp
@@ -5,13 +5,5 @@ using namespace std;
 
 int main()
 {
-    // vector<int>vc(10, -1);
-     int arr[20];
-     int sizeofarr = sizeof(arr)/sizeof(int);
     
-//   fill(vc.begin(), vc.end(), 8);
-    fill(arr, arr+sizeofarr, 7);
-    cout<<arr[5];
-
-    return 0;
 }
